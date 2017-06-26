04bd092e655afc4b49870d9abefd7b1c5a761dee
Kristoffer Svensson <ks222rt@student.lnu.se>
2017-06-26 18:46:10 +0200

    began to work with the sign in to twitter

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
-    
     res.redirect('http://' + req.hostname + req.url);
   } else {
     next();  
   }
+});
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.options('*', cors())
