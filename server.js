99a7847c53454780ad7b024f579c682584adc045
Kristoffer Svensson <ks222rt@student.lnu.se>
2017-06-26 18:48:02 +0200

    began to work with the sign in to twitter

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
@@ -13,14 +14,6 @@ app.use(function (req, res, next){
   }
 });
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
-app.options('*', cors())
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
