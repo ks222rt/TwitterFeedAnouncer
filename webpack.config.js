782a72108436424d4677cf556da5927c933838f7
Kristoffer Svensson <ks222rt@student.lnu.se>
2017-06-26 14:58:07 +0200

    testing to change the server file

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     })
   ],
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js',
     publicPath: '/public/'
   },
